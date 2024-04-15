ba23ce3590be5ec07d07bdd7f8e196fc5680698e
byron00123 <omondibyron25@gmail.com>
2024-04-15 13:01:40 +0300

    update price list

--- price_management_system/prices/forms.py
+++ price_management_system/prices/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Price
 
 class PriceForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        product_choices = kwargs.pop('product_choices', None)
+        super(PriceForm, self).__init__(*args, **kwargs)
+        if product_choices:
+            self.fields['product'].queryset = product_choices
+
     class Meta:
         model = Price
         fields = ['product', 'farmgate_price', 'retail_price', 'date', 'location']
--- price_management_system/prices/views.py
+++ price_management_system/prices/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
-
-from .models import Price 
-from .forms import PriceForm  # Import your PriceForm
+from django.shortcuts import render, redirect
+from .models import Price, Product
+from .forms import PriceForm
 
 def price_list(request):
-    # Assuming you have a PriceForm defined in forms.py
-    form = PriceForm()
+    # Retrieve existing products from the database
+    products = Product.objects.all()
 
-    # You may have other logic to retrieve the prices from the database
+    # Check if the form is submitted
+    if request.method == 'POST':
+        # Create a PriceForm instance with the submitted data
+        form = PriceForm(request.POST, product_choices=products)
+        # Check if the form is valid
+        if form.is_valid():
+            # Save the form data to the database
+            form.save()
+            # Redirect to the price list page to prevent form resubmission on refresh
+            return redirect('price_list')
+    else:
+        # If the request method is GET, create an empty PriceForm instance
+        form = PriceForm(product_choices=products)
+
+    # Retrieve existing prices from the database
     prices = Price.objects.all()
 
+    # Render the price list template with the form and prices
     return render(request, 'prices/price_list.html', {'form': form, 'prices': prices})
